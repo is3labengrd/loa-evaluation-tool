f270821bb138b1bc43640a5cfbf0bf6c720d8940
Drobniac <drobniac@gmail.com>
2018-08-28 17:49:38 +0200

    Set up Spring Boot

---
+++ src/main/java/eng/it/loatool/EngLoatoolApplication.java
@@ -0,0 +1,12 @@
+package eng.it.loatool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngLoatoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EngLoatoolApplication.class, args);
+    }
+}
---
+++ src/test/java/eng/it/loatool/EngLoatoolApplicationTests.java
@@ -0,0 +1,16 @@
+package eng.it.loatool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EngLoatoolApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
