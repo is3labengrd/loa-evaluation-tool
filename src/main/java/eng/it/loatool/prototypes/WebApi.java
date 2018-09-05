6cd33e52c82e1694e47580ba61a9f9f47f398470
Drobniac <drobniac@gmail.com>
2018-09-05 12:55:19 +0200

    Update prototype WebApi

--- src/main/java/eng/it/loatool/prototypes/WebApi.java
+++ src/main/java/eng/it/loatool/prototypes/WebApi.java
@@ -3,6 +3,13 @@ package eng.it.loatool.prototypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import entities.TbAceProSeq;
+import entities.TbAceSubProLev;
 
 @Controller
 public class WebApi {
@@ -12,9 +19,46 @@ public class WebApi {
         return ResponseEntity.ok(ProcessList.getExample());
     }
 
-    @GetMapping("/proto/test")
-    public ResponseEntity<String> test() {
-        return ResponseEntity.ok("Test");
+    @GetMapping("/proto/processes/{processId}")
+    public ResponseEntity<Void> getProcess(@PathVariable("processId") String processId) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("/proto/processes")
+    public ResponseEntity<Void> createProcess(
+        @PathVariable("processId") String processId,
+        @RequestBody TbAceProSeq body
+    ) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/proto/processes/{processId}")
+    public ResponseEntity<Void> updateProcess(
+        @PathVariable("processId") String processId,
+        @RequestBody TbAceProSeq body
+    ) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/proto/sub-processes/{subprocessId}")
+    public ResponseEntity<Void> getSubProcess(@PathVariable("processId") String processId) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("/proto/sub-processes")
+    public ResponseEntity<Void> createSubProcess(
+        @PathVariable("processId") String processId,
+        @RequestBody TbAceSubProLev body
+    ) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/proto/sub-processes/{subprocessId}")
+    public ResponseEntity<Void> updateSubProcess(
+        @PathVariable("processId") String processId,
+        @RequestBody TbAceSubProLev body
+    ) {
+        return ResponseEntity.notFound().build();
     }
 
 }
