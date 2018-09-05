510b37c984b21cf40a6238cb25c479ba00c14fe5
Drobniac <drobniac@gmail.com>
2018-09-05 16:19:34 +0200

    Fix codestyle and typo in TbAceSubProLev

--- src/main/java/entities/TbAceSubProLev.java
+++ src/main/java/entities/TbAceSubProLev.java
@@ -1,11 +1,13 @@
 package entities;
 // Generated Sep 4, 2018 12:58:34 PM by Hibernate Tools 5.2.10.Final
 
+import static javax.persistence.GenerationType.IDENTITY;
+
 import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import static javax.persistence.GenerationType.IDENTITY;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -18,93 +20,94 @@ import javax.persistence.TemporalType;
 @Table(name = "TB_ACE_SUB_PRO_LEV", catalog = "loa_evaluation_tool")
 public class TbAceSubProLev implements java.io.Serializable {
 
-	private Integer pkTbId;
-	private int fkTbAceProSeq;
-	private String name;
-	private int proLevel;
-	private String varProSeqId;
-	private Date createDate;
-	private Date udpateDate;
+    private Integer pkTbId;
+    private int fkTbAceProSeq;
+    private String name;
+    private int proLevel;
+    private String varProSeqId;
+    private Date createDate;
+    private Date updateDate;
 
-	public TbAceSubProLev() {
-	}
+    public TbAceSubProLev() {
+    }
 
-	public TbAceSubProLev(int fkTbAceProSeq, String name, int proLevel, String varProSeqId, Date createDate,
-			Date udpateDate) {
-		this.fkTbAceProSeq = fkTbAceProSeq;
-		this.name = name;
-		this.proLevel = proLevel;
-		this.varProSeqId = varProSeqId;
-		this.createDate = createDate;
-		this.udpateDate = udpateDate;
-	}
+    public TbAceSubProLev(
+        int fkTbAceProSeq, String name, int proLevel, String varProSeqId, Date createDate, Date updateDate
+    ) {
+        this.fkTbAceProSeq = fkTbAceProSeq;
+        this.name = name;
+        this.proLevel = proLevel;
+        this.varProSeqId = varProSeqId;
+        this.createDate = createDate;
+        this.updateDate = updateDate;
+    }
 
-	@Id
-	@GeneratedValue(strategy = IDENTITY)
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
 
-	@Column(name = "PK_TB_ID", unique = true, nullable = false)
-	public Integer getPkTbId() {
-		return this.pkTbId;
-	}
+    @Column(name = "PK_TB_ID", unique = true, nullable = false)
+    public Integer getPkTbId() {
+        return this.pkTbId;
+    }
 
-	public void setPkTbId(Integer pkTbId) {
-		this.pkTbId = pkTbId;
-	}
+    public void setPkTbId(Integer pkTbId) {
+        this.pkTbId = pkTbId;
+    }
 
-	@Column(name = "FK_TB_ACE_PRO_SEQ", nullable = false)
-	public int getFkTbAceProSeq() {
-		return this.fkTbAceProSeq;
-	}
+    @Column(name = "FK_TB_ACE_PRO_SEQ", nullable = false)
+    public int getFkTbAceProSeq() {
+        return this.fkTbAceProSeq;
+    }
 
-	public void setFkTbAceProSeq(int fkTbAceProSeq) {
-		this.fkTbAceProSeq = fkTbAceProSeq;
-	}
+    public void setFkTbAceProSeq(int fkTbAceProSeq) {
+        this.fkTbAceProSeq = fkTbAceProSeq;
+    }
 
-	@Column(name = "NAME", nullable = false, length = 45)
-	public String getName() {
-		return this.name;
-	}
+    @Column(name = "NAME", nullable = false, length = 45)
+    public String getName() {
+        return this.name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	@Column(name = "PRO_LEVEL", nullable = false)
-	public int getProLevel() {
-		return this.proLevel;
-	}
+    @Column(name = "PRO_LEVEL", nullable = false)
+    public int getProLevel() {
+        return this.proLevel;
+    }
 
-	public void setProLevel(int proLevel) {
-		this.proLevel = proLevel;
-	}
+    public void setProLevel(int proLevel) {
+        this.proLevel = proLevel;
+    }
 
-	@Column(name = "VAR_PRO_SEQ_ID", nullable = false, length = 45)
-	public String getVarProSeqId() {
-		return this.varProSeqId;
-	}
+    @Column(name = "VAR_PRO_SEQ_ID", nullable = false, length = 45)
+    public String getVarProSeqId() {
+        return this.varProSeqId;
+    }
 
-	public void setVarProSeqId(String varProSeqId) {
-		this.varProSeqId = varProSeqId;
-	}
+    public void setVarProSeqId(String varProSeqId) {
+        this.varProSeqId = varProSeqId;
+    }
 
-	@Temporal(TemporalType.TIMESTAMP)
-	@Column(name = "CREATE_DATE", nullable = false, length = 19)
-	public Date getCreateDate() {
-		return this.createDate;
-	}
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "CREATE_DATE", nullable = false, length = 19)
+    public Date getCreateDate() {
+        return this.createDate;
+    }
 
-	public void setCreateDate(Date createDate) {
-		this.createDate = createDate;
-	}
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
 
-	@Temporal(TemporalType.TIMESTAMP)
-	@Column(name = "UDPATE_DATE", nullable = false, length = 19)
-	public Date getUdpateDate() {
-		return this.udpateDate;
-	}
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "UDPATE_DATE", nullable = false, length = 19)
+    public Date getUpdateDate() {
+        return this.updateDate;
+    }
 
-	public void setUdpateDate(Date udpateDate) {
-		this.udpateDate = udpateDate;
-	}
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
 
 }
