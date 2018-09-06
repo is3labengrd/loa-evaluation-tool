d866e681db9923d9abbd93c79f1ba8c5340f77f9
Drobniac <drobniac@gmail.com>
2018-09-06 14:29:39 +0200

    Make getProcesses method transactional in GetProcessesService

--- src/main/java/eng/it/loatool/prototypes/GetProcessesService.java
+++ src/main/java/eng/it/loatool/prototypes/GetProcessesService.java
@@ -2,12 +2,14 @@ package eng.it.loatool.prototypes;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import eng.it.loatool.entities.TbAceProSeq;
 
 @Service
 public class GetProcessesService {
 
+    @Transactional
     Iterable<TbAceProSeq> getProcesses() {
         return tbAceProSeqRepository.findAll();
     }
