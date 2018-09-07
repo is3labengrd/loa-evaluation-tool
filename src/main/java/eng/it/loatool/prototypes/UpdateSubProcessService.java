2bfc6fc32abd0925c85c5934b99edd8a416dcded
Drobniac <drobniac@gmail.com>
2018-09-07 11:51:24 +0200

    Fix UpdateSubProcessService
    UpdateSubProcessService can now only update existing subprocesses

--- src/main/java/eng/it/loatool/prototypes/UpdateSubProcessService.java
+++ src/main/java/eng/it/loatool/prototypes/UpdateSubProcessService.java
@@ -1,5 +1,7 @@
 package eng.it.loatool.prototypes;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,10 +13,15 @@ import eng.it.loatool.subprocess_level.SubProcessLevelRepository;
 public class UpdateSubProcessService {
 
     @Transactional
-    public SubProcessLevel updateSubProcess(Integer processId, SubProcessLevel tbAceSubProLev) {
-        tbAceSubProLev.setPkTbId(processId);
+    public Optional<SubProcessLevel> updateSubProcess(Integer processId, SubProcessLevel tbAceSubProLev) {
+        if (
+            tbAceSubProLev.getPkTbId() == null ||
+            !tbAceSubProLevRepository.existsById(tbAceSubProLev.getPkTbId())
+        ) {
+            return Optional.empty();
+        }
         tbAceSubProLevRepository.save(tbAceSubProLev);
-        return tbAceSubProLev;
+        return Optional.of(tbAceSubProLev);
     }
 
     @Autowired private SubProcessLevelRepository tbAceSubProLevRepository;
