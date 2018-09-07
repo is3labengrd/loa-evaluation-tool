d34aa63c1069322efb80a09ec2a61b12c663b62c
Drobniac <drobniac@gmail.com>
2018-09-07 12:41:18 +0200

    Create ResponseEntityTransformer

---
+++ src/main/java/eng/it/util/ResponseEntityTransformer.java
@@ -0,0 +1,20 @@
+package eng.it.util;
+
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+
+public class ResponseEntityTransformer {
+
+    public static <T> ResponseEntity<T> transform(Optional<T> optional) {
+        if (optional.isPresent()) {
+            return ResponseEntity.ok(optional.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    public static <T> ResponseEntity<T> transformOk(T object) {
+        return ResponseEntity.ok(object);
+    }
+
+}
