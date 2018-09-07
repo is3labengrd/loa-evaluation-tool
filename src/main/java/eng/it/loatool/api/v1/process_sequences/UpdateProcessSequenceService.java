fe05925a22c66b5d8d91ed820fd8a026f34edd77
Drobniac <drobniac@gmail.com>
2018-09-07 12:40:35 +0200

    Fix bug in UpdateProcessSequenceService

---
+++ src/main/java/eng/it/loatool/api/v1/process_sequences/UpdateProcessSequenceService.java
@@ -0,0 +1,27 @@
+package eng.it.loatool.api.v1.process_sequences;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import eng.it.loatool.process_sequence.ProcessSequence;
+import eng.it.loatool.process_sequence.ProcessSequenceRepository;
+
+@Service
+public class UpdateProcessSequenceService {
+
+    @Transactional
+    public Optional<ProcessSequence> updateProcess(Integer processId, ProcessSequence tbAceProSeq) {
+        tbAceProSeq.setPkTbId(processId);
+        if (processId == null || !tbAceProSeqRepository.existsById(processId)) {
+            return Optional.empty();
+        }
+        tbAceProSeqRepository.save(tbAceProSeq);
+        return Optional.of(tbAceProSeq);
+    }
+
+    @Autowired private ProcessSequenceRepository tbAceProSeqRepository;
+
+}
