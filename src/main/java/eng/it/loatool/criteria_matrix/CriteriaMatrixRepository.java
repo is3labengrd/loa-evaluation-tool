92201705533e894ebe62fb9fa066c690d9a53628
Drobniac <drobniac@gmail.com>
2018-09-10 12:53:03 +0200

    Define CriteriaMatrixRepository

---
+++ src/main/java/eng/it/loatool/criteria_matrix/CriteriaMatrixRepository.java
@@ -0,0 +1,8 @@
+package eng.it.loatool.criteria_matrix;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CriteriaMatrixRepository extends CrudRepository<CriteriaMatrix, Integer> {
+}
