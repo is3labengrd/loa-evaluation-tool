5dde4fdfd4943580389b52ae47c85415205dd9de
Drobniac <drobniac@gmail.com>
2018-09-10 14:59:30 +0200

    Fix code in CreateCriteriaMatrixService

--- src/main/java/eng/it/loatool/api/v1/criteria_matrices/CreateCriteriaMatrixService.java
+++ src/main/java/eng/it/loatool/api/v1/criteria_matrices/CreateCriteriaMatrixService.java
@@ -3,15 +3,17 @@ package eng.it.loatool.api.v1.criteria_matrices;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import eng.it.loatool.criteria_matrix.CriteriaMatrix;
 import eng.it.loatool.criteria_matrix.CriteriaMatrixRepository;
 
+@Service
 public class CreateCriteriaMatrixService {
 
     @Transactional
-    public Optional<CriteriaMatrix> CreateCriteriaMatrix(CriteriaMatrix criteriaMatrix) {
+    public Optional<CriteriaMatrix> createCriteriaMatrix(CriteriaMatrix criteriaMatrix) {
         if (
             criteriaMatrix.getPkTbId() == null ||
             !criteriaMatrixRepository.existsById(criteriaMatrix.getPkTbId())
