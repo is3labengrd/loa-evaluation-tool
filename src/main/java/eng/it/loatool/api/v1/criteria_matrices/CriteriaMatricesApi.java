08e8427ca13539c09af3fa053d912fe32601a502
Drobniac <drobniac@gmail.com>
2018-09-10 15:01:32 +0200

    Implement CriteriaMatricesApi

--- src/main/java/eng/it/loatool/api/v1/criteria_matrices/CriteriaMatricesApi.java
+++ src/main/java/eng/it/loatool/api/v1/criteria_matrices/CriteriaMatricesApi.java
@@ -1,5 +1,6 @@
 package eng.it.loatool.api.v1.criteria_matrices;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,23 +10,30 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import eng.it.loatool.criteria_matrix.CriteriaMatrix;
+import eng.it.util.ResponseEntityTransformer;
 
 @Controller
 public class CriteriaMatricesApi {
 
     @GetMapping("/v1/criteria-matrices")
     public ResponseEntity<?> getCriteriaMatrices() {
-        return null;
+        return ResponseEntityTransformer.transformOk(
+            getCriteriaMatricesService.getCriteriaMatrices()
+        );
     }
 
     @GetMapping("/v1/criteria-matrices/{matrixId}")
     public ResponseEntity<?> getCriteriaMatrix(@PathVariable("matrixId") Integer matrixId) {
-        return null;
+        return ResponseEntityTransformer.transform(
+            getCriteriaMatrixService.getCriteriaMatrix(matrixId)
+        );
     }
 
     @PostMapping("/v1/criteria-matrices")
     public ResponseEntity<?> createCriteriaMatrix(@RequestBody CriteriaMatrix body) {
-        return null;
+        return ResponseEntityTransformer.transform(
+            createCriteriaMatrixService.createCriteriaMatrix(body)
+        );
     }
 
     @PutMapping("/v1/criteria-matrices/{matrixId}")
@@ -33,7 +41,14 @@ public class CriteriaMatricesApi {
         @PathVariable("matrixId") Integer matrixId,
         @RequestBody CriteriaMatrix body
     ) {
-        return null;
+        return ResponseEntityTransformer.transform(
+            updateCriteriaMatrixService.updateCriteriaMatrix(matrixId, body)
+        );
     }
+
+    @Autowired private UpdateCriteriaMatrixService updateCriteriaMatrixService;
+    @Autowired private CreateCriteriaMatrixService createCriteriaMatrixService;
+    @Autowired private GetCriteriaMatricesService getCriteriaMatricesService;
+    @Autowired private GetCriteriaMatrixService getCriteriaMatrixService;
 
 }
