139f3a55b5d81d7dc1ce7a756ea4f12d8de4a294
Drobniac <drobniac@gmail.com>
2018-09-11 09:34:30 +0200

    Define CognitiveLOARepository

---
+++ src/main/java/eng/it/loatool/cognitive_loa/CognitiveLOARepository.java
@@ -0,0 +1,8 @@
+package eng.it.loatool.cognitive_loa;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CognitiveLOARepository extends CrudRepository<CognitiveLOA, Integer> {
+}
