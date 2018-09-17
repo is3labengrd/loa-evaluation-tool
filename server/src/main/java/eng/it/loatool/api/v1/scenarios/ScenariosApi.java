c72ffda42428ebdebe55d29e1029b137e3d0e164
Drobniac <drobniac@gmail.com>
2018-09-17 15:30:49 +0200

    Update ScenariosApi
    I added create and get services for scenarios.

--- server/src/main/java/eng/it/loatool/api/v1/scenarios/ScenariosApi.java
+++ server/src/main/java/eng/it/loatool/api/v1/scenarios/ScenariosApi.java
@@ -1,17 +1,24 @@
 package eng.it.loatool.api.v1.scenarios;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import eng.it.loatool.scenario.Scenario;
+import eng.it.util.ResponseEntityTransformer;
 
 @Controller
 public class ScenariosApi {
 
     @GetMapping("/v1/scenarios")
     public ResponseEntity<?> getScenarios() {
-        return null;
+        return ResponseEntityTransformer.transformOk(
+            getScenariosService.getScenarios()
+        );
     }
 
     @GetMapping("/v1/scenarios/{scenarioId}")
@@ -20,13 +27,20 @@ public class ScenariosApi {
     }
 
     @PostMapping("/v1/scenarios")
-    public ResponseEntity<?> createScenario() {
-        return null;
+    public ResponseEntity<?> createScenario(
+        @RequestBody Scenario body
+    ) {
+        return ResponseEntityTransformer.transform(
+            createScenarioService.createScenario(body)
+        );
     }
 
     @PutMapping("/v1/scenarios/{scenarioId}")
     public ResponseEntity<?> updateScenario() {
         return null;
     }
+
+    @Autowired private GetScenariosService getScenariosService;
+    @Autowired private CreateScenarioService createScenarioService;
 
 }
