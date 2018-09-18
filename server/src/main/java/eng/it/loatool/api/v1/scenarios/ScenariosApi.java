c72e1cedbde055d771866da9e79241a341ac2a3d
Drobniac <drobniac@gmail.com>
2018-09-18 13:04:47 +0200

    Update ScenariosApi

--- server/src/main/java/eng/it/loatool/api/v1/scenarios/ScenariosApi.java
+++ server/src/main/java/eng/it/loatool/api/v1/scenarios/ScenariosApi.java
@@ -4,12 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import eng.it.loatool.GeneralCRUDService;
 import eng.it.loatool.api.ResponseEntityTransformer;
 import eng.it.loatool.scenario.Scenario;
+import eng.it.loatool.scenario.ScenarioRepository;
 
 @Controller
 public class ScenariosApi {
@@ -22,8 +25,12 @@ public class ScenariosApi {
     }
 
     @GetMapping("/v1/scenarios/{scenarioId}")
-    public ResponseEntity<?> getScenario() {
-        return null;
+    public ResponseEntity<?> getScenario(
+        @PathVariable("scenarioId") Integer id
+    ) {
+        return ResponseEntityTransformer.transform(
+            generalCRUDService.getOne(scenarioRepository, id)
+        );
     }
 
     @PostMapping("/v1/scenarios")
@@ -36,11 +43,18 @@ public class ScenariosApi {
     }
 
     @PutMapping("/v1/scenarios/{scenarioId}")
-    public ResponseEntity<?> updateScenario() {
-        return null;
+    public ResponseEntity<?> updateScenario(
+        @PathVariable("scenarioId") Integer id,
+        @RequestBody Scenario body
+    ) {
+        return ResponseEntityTransformer.transform(
+            generalCRUDService.update(scenarioRepository, id, body)
+        );
     }
 
     @Autowired private GetScenariosService getScenariosService;
     @Autowired private CreateScenarioService createScenarioService;
+    @Autowired private GeneralCRUDService generalCRUDService;
+    @Autowired private ScenarioRepository scenarioRepository;
 
 }
