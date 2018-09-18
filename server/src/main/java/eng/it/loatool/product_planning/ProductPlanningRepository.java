369820f4cbf87eb7588b6bee6d6525ddb38ec8a8
Drobniac <drobniac@gmail.com>
2018-09-18 15:43:28 +0200

    Fix missing repository annotations

--- server/src/main/java/eng/it/loatool/product_planning/ProductPlanningRepository.java
+++ server/src/main/java/eng/it/loatool/product_planning/ProductPlanningRepository.java
@@ -1,6 +1,8 @@
 package eng.it.loatool.product_planning;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductPlanningRepository extends CrudRepository<ProductPlanning, Integer> {
 }
