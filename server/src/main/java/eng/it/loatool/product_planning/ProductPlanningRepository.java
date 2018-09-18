fcdde545b87a696d515c0ca4ea2c876cfe824ef3
Drobniac <drobniac@gmail.com>
2018-09-18 15:23:33 +0200

    Implement and expose ProductPlanningApi

---
+++ server/src/main/java/eng/it/loatool/product_planning/ProductPlanningRepository.java
@@ -0,0 +1,6 @@
+package eng.it.loatool.product_planning;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductPlanningRepository extends CrudRepository<ProductPlanning, Integer> {
+}
