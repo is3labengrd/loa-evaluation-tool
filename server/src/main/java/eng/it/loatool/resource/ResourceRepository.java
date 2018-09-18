682fac4bd24e2b9046dcd1285531ce2853f2108e
Drobniac <drobniac@gmail.com>
2018-09-18 15:50:32 +0200

    Define ResourceRepository

---
+++ server/src/main/java/eng/it/loatool/resource/ResourceRepository.java
@@ -0,0 +1,8 @@
+package eng.it.loatool.resource;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ResourceRepository extends CrudRepository<Resource, TbAceResId>{
+}
