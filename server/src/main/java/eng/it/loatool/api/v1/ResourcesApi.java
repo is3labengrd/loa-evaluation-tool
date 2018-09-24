f268b42c8a13a00eb75f990be3a4ac7ad311d2c9
Drobniac <drobniac@gmail.com>
2018-09-24 14:58:03 +0200

    Fix ResourcesApi method names

--- server/src/main/java/eng/it/loatool/api/v1/ResourcesApi.java
+++ server/src/main/java/eng/it/loatool/api/v1/ResourcesApi.java
@@ -18,28 +18,28 @@ import eng.it.loatool.resource.ResourceRepository;
 public class ResourcesApi {
 
     @GetMapping("/v1/resources")
-    public ResponseEntity<?> getEveryProductPlanning() {
+    public ResponseEntity<?> getEveryResource() {
         return ResponseEntityTransformer.transformOk(
             generalCRUDService.getAll(resourceRepository)
         );
     }
 
     @GetMapping("/v1/resources/{id}")
-    public ResponseEntity<?> getProductPlanning(@PathVariable("id") Integer id) {
+    public ResponseEntity<?> getResource(@PathVariable("id") Integer id) {
         return ResponseEntityTransformer.transform(
             generalCRUDService.getOne(resourceRepository, id)
         );
     }
 
     @PostMapping("/v1/resources")
-    public ResponseEntity<?> createProductPlanning(@RequestBody Resource body) {
+    public ResponseEntity<?> createResource(@RequestBody Resource body) {
         return ResponseEntityTransformer.transform(
             generalCRUDService.create(resourceRepository, body)
         );
     }
 
     @PutMapping("/v1/resources/{id}")
-    public ResponseEntity<?> updateProductPlanning(
+    public ResponseEntity<?> updateResource(
         @PathVariable("id") Integer id,
         @RequestBody Resource body
     ) {
