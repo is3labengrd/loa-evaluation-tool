fcdc68021550ff92d17d9a5c4fcc177edd530f7c
Drobniac <drobniac@gmail.com>
2018-09-27 10:45:45 +0200

    Define SubScenarioMatrix

---
+++ server/src/main/java/eng/it/loatool/limbo/SubScenarioMatrix.java
@@ -0,0 +1,40 @@
+package eng.it.loatool.limbo;
+
+
+public class SubScenarioMatrix {
+
+    public int loaPhysical;
+    public int loaCognitive;
+    public int processTime;
+    public int numberOfShiftsPerDay;
+    public int hoursPerShift;
+    public int workingDaysPerYear;
+    public int hoursPerYear;
+    public int numberOfProducedPiecesInUnitsPerHour;
+    public int numberOfProducedPiecesinUnitsPerYear;
+    public int proportionalWageCostsPerHour;
+    public int numberOfOperatorsPerMachine;
+    public int rateOfParticipationPerYear;
+    public int labourCost;
+    public int electricityConsumption;
+    public int electricityConsumptionOnStandby;
+    public int electricityPrice;
+    public int energyCost;
+    public int annualMaintenanceCostsDirectEntry;
+    public int annualMaintenanceCostsEstimate;
+    public int maintenanceCosts;
+    public int installationSurface;
+    public int costsOfSquareMetrePerMonth;
+    public int annualSpaceCosts;
+    public int machinePurchaseValue;
+    public int machineSalesValue;
+    public int economicUsefulLife;
+    public int imputedDepreciation;
+    public int interestRate;
+    public int accruedInterestCosts;
+    public int variableCostsTotal;
+    public int fixedCostsTotal;
+    public int assemblyCostsPerPiece;
+    public int assemblyCostsTotal;
+
+}
