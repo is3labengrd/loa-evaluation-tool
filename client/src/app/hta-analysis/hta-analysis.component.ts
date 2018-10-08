09421d113d53a1959dbe2617208f2786dff7779a
Drobniac <drobniac@gmail.com>
2018-10-08 15:16:39 +0200

    Implement cell selection and binding for dimensional stability

--- client/src/app/hta-analysis/hta-analysis.component.ts
+++ client/src/app/hta-analysis/hta-analysis.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HTAAnalysisComponent implements OnInit {
 
+  criteriaMatrix = {
+    "dimensionalStability": null
+  };
+
   constructor() { }
 
   ngOnInit() {
