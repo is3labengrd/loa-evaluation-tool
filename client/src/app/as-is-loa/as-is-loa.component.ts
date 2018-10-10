a37fe4205766e742a7d33026ef2ce0fe0cce3190
Drobniac <drobniac@gmail.com>
2018-10-10 16:00:04 +0200

    Remove unnecessary comments from AsIsLOAComponent

--- client/src/app/as-is-loa/as-is-loa.component.ts
+++ client/src/app/as-is-loa/as-is-loa.component.ts
@@ -14,46 +14,6 @@ export class AsIsLOAComponent implements OnInit {
   BablChart = [];
 
   ngOnInit() {
-    // this.BarChart = new Chart('barChart', {
-    //   type: 'bar',
-    //   data: {
-    //   labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    //   datasets: [{
-    //       label: '# of Votes',
-    //       data: [9, 7, 3, 5, 2, 10],
-    //       backgroundColor: [
-    //           'rgba(255, 99, 132, 0.2)',
-    //           'rgba(54, 162, 235, 0.2)',
-    //           'rgba(255, 206, 86, 0.2)',
-    //           'rgba(75, 192, 192, 0.2)',
-    //           'rgba(153, 102, 255, 0.2)',
-    //           'rgba(255, 159, 64, 0.2)'
-    //       ],
-    //       borderColor: [
-    //           'rgba(255,99,132,1)',
-    //           'rgba(54, 162, 235, 1)',
-    //           'rgba(255, 206, 86, 1)',
-    //           'rgba(75, 192, 192, 1)',
-    //           'rgba(153, 102, 255, 1)',
-    //           'rgba(255, 159, 64, 1)'
-    //       ],
-    //       borderWidth: 1
-    //   }]
-    //   },
-    //   options: {
-    //   title:{
-    //       text:"Bar Chart",
-    //       display:true
-    //   },
-    //   scales: {
-    //       yAxes: [{
-    //           ticks: {
-    //               beginAtZero:true
-    //           }
-    //       }]
-    //   }
-    //   }
-    // });
     this.BablChart = new Chart('bablChart', {
       type: 'bubble',
       data: {
