cfafa531b0239cd4252d5d456df80ee229f179cf
Drobniac <drobniac@gmail.com>
2018-10-11 17:56:54 +0200

    Promote ResourceRepository to PagingAndSortingRepository

--- server/src/main/java/eng/it/loatool/resource/ResourceRepository.java
+++ server/src/main/java/eng/it/loatool/resource/ResourceRepository.java
@@ -1,8 +1,8 @@
 package eng.it.loatool.resource;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ResourceRepository extends CrudRepository<Resource, Integer>{
+public interface ResourceRepository extends PagingAndSortingRepository<Resource, Integer> {
 }
