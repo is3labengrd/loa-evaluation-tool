98db47700365474c5e778cf002a1481dbea83da2
mmattia17 <mattia.marzano@demetrix.it>
2018-10-11 11:44:58 +0200

    Added VARApi services

--- server/src/main/java/eng/it/loatool/api/v1/VARApi.java
+++ server/src/main/java/eng/it/loatool/api/v1/VARApi.java
@@ -1,9 +1,13 @@
 package eng.it.loatool.api.v1;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import eng.it.loatool.var.service.VARServiceWrapper;
+
+import java.io.IOException;
 
 @RestController
 public class VARApi {
@@ -11,5 +15,35 @@ public class VARApi {
     @RequestMapping("/v1/var/process-segments")
     public Object getVarProcessSegmentList() {
         return VARServiceWrapper.getProcessesSegmentList();
+    }
+
+    @RequestMapping(value = "/v1/var/addResource", method = RequestMethod.POST)
+    public void addResource (@RequestBody String json) throws IOException {
+
+        VARServiceWrapper.addResource(json);
+    }
+
+    @RequestMapping(value = "/v1/var/editResource", method = RequestMethod.PUT)
+    public void editResource (@RequestBody String json) throws IOException {
+
+        VARServiceWrapper.editResource(json);
+    }
+
+    @RequestMapping(value = "/v1/var/editProductPlanning", method = RequestMethod.PUT)
+    public void editProductPlanning (@RequestBody String json) throws IOException {
+
+        VARServiceWrapper.editProductPlanning(json);
+    }
+
+    @RequestMapping(value = "/v1/var/editProcessSpecificInformation", method = RequestMethod.PUT)
+    public void editProcessSpecificInformation (@RequestBody String json) throws IOException {
+
+        VARServiceWrapper.editProcessSpecificInformation(json);
+    }
+
+    @RequestMapping(value = "/v1/var/editProportionalWageCost", method = RequestMethod.PUT)
+    public void editProportionalWageCost (@RequestBody String json) throws IOException {
+
+        VARServiceWrapper.editProportionalWageCost(json);
     }
 }
