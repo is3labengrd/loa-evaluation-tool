27253fbfca19b670b2f35412366acffef9d3a753
Drobniac <drobniac@gmail.com>
2018-10-29 11:45:29 +0100

    Use CookieService instead of raw document.cookie

--- client/src/app/process-list/process-list.component.ts
+++ client/src/app/process-list/process-list.component.ts
@@ -31,13 +31,13 @@ export class ProcessListComponent implements OnInit {
   }
 
   pageIterable = {
-    [Symbol.iterator]: () => {}
+    [Symbol.iterator]: () => { }
   };
 
   constructor(
     private http: HttpClient,
     private _processListService: CookieService
-  ) {}
+  ) { }
 
   ngOnInit() {
     this._processListService.deleteCookie("selectedSubprocess");
@@ -47,7 +47,7 @@ export class ProcessListComponent implements OnInit {
       .post(environment.apiUrl + '/v1/var/populate-process-segments', {})
       .toPromise()
       .then(
-        () => {this.populateProcessSegmentList();}
+        () => { this.populateProcessSegmentList(); }
       )
   }
 
@@ -113,37 +113,23 @@ export class ProcessListComponent implements OnInit {
   }
 
   saveAnalysisData(data, subProcessData) {
-    let skimmedData = {
-      mainProcess: data.mainProcess
-    }
-    let stringifiedData = JSON.stringify(skimmedData);
-    document.cookie=`it.eng.loatool.analysisData=${stringifiedData}`;
-
-
-
-
-    //create cookie containing the specific subprocess info
     var actualSubProcessInfo = {};
-
-    actualSubProcessInfo['mainProcessName']= subProcessData.name;
-    actualSubProcessInfo['mainProcessId']= subProcessData.rawElementReference.mainProcess.pkTbId;
-    actualSubProcessInfo['subLevels']= subProcessData.sublevels;
-    actualSubProcessInfo['totalNumberSubprocs']  = data.mainProcess.subprocessLevels.length;
-
-  for (var i = 1; i <= subProcessData.sublevels; i++) {
-     if(!("undefined" === typeof(subProcessData.rawElementReference['subProcessLevel'+i])) && subProcessData.rawElementReference['subProcessLevel'+i]!=null )
-     actualSubProcessInfo['level'+i] = new SubProcess(subProcessData.rawElementReference['subProcessLevel'+i].pkTbId, subProcessData.rawElementReference['subProcessLevel'+i].name);
+    actualSubProcessInfo['mainProcessName'] = subProcessData.name;
+    actualSubProcessInfo['mainProcessId'] = subProcessData.rawElementReference.mainProcess.pkTbId;
+    actualSubProcessInfo['subLevels'] = subProcessData.sublevels;
+    actualSubProcessInfo['totalNumberSubprocs'] = data.mainProcess.subprocessLevels.length;
+    for (var i = 1; i <= subProcessData.sublevels; i++) {
+      if (!("undefined" === typeof (subProcessData.rawElementReference['subProcessLevel' + i])) && subProcessData.rawElementReference['subProcessLevel' + i] != null)
+        actualSubProcessInfo['level' + i] = new SubProcess(subProcessData.rawElementReference['subProcessLevel' + i].pkTbId, subProcessData.rawElementReference['subProcessLevel' + i].name);
+    }
+    this._processListService.setCookie("selectedSubprocess", JSON.stringify(actualSubProcessInfo), 1, "");
   }
-
-    this._processListService.setCookie("selectedSubprocess",JSON.stringify(actualSubProcessInfo),1,"");
-
- }
 
 }
 
-function SubProcess(id, name){
-    this.id = id;
-    this.name = name;
+function SubProcess(id, name) {
+  this.id = id;
+  this.name = name;
 }
 
 
