03306ea36550ddb9d216087e2f8f3476b406f011
Drobniac <drobniac@gmail.com>
2018-10-30 13:01:19 +0100

    Complete hta-analysis

--- server/src/main/java/eng/it/loatool/criteria_matrix/CriteriaMatrixRepository.java
+++ server/src/main/java/eng/it/loatool/criteria_matrix/CriteriaMatrixRepository.java
@@ -1,8 +1,16 @@
 package eng.it.loatool.criteria_matrix;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CriteriaMatrixRepository extends CrudRepository<CriteriaMatrix, Integer> {
+
+    @Query("from CriteriaMatrix where fkTbAceSubProLev=:subprocessId")
+    public Optional<CriteriaMatrix> getCriteriaMatrixBySubprocessLevel(@Param("subprocessId") Integer subprocessId);
+
 }
