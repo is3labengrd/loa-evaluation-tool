a3a9c42b3833ae630747be13b49ec1148b6d10d7
unknown <arspida@gmail.com>
2018-10-31 15:11:20 +0100

    Modify hta-analysis component

--- client/src/app/hta-analysis/hta-analysis.component.ts
+++ client/src/app/hta-analysis/hta-analysis.component.ts
@@ -14,111 +14,121 @@ import { initializeMatrix, updateMatrix } from './matrix';
 export class HTAAnalysisComponent implements OnInit {
 
   constructor(
-    private http:HttpClient,
-    private route:ActivatedRoute,
-    private cookieService:CookieService
+    private http: HttpClient,
+    private route: ActivatedRoute,
+    private cookieService: CookieService
   ) { }
 
   selectedSubprocess = this.cookieService
-    .getCookie("selectedSubprocess");
+    .getCookie('selectedSubprocess');
   subprocessId = this.selectedSubprocess[
-    "level" + this.selectedSubprocess.maxDepth
+    'level' + this.selectedSubprocess.maxDepth
   ].id;
 
-  matrixRequest:any = {
-    "fkTbAceSubProLev": this.subprocessId,
-    "dsStable": 0,
-    "dsReducedStability": 0,
-    "dsHandlyStable": 0,
-    "dsUnstable": 0,
-    "stInsensitive": 0,
-    "stHardlySensitive": 0,
-    "stSensitive": 0,
-    "stVerySensitive": 0,
-    "grExGripSurface": 0,
-    "grIntGripSurface": 0,
-    "grMagneticGripper": 0,
-    "grFabClosureGripper": 0,
-    "nvNoFurtherVariants": 0,
-    "nvOneFurtherVariants": 0,
-    "nvTwoFurtherVariants": 0,
-    "nvMoreFurtherVariants": 0,
-    "spUp": 0,
-    "spMore": 0,
-    "spStableUnstable": 0,
-    "spOnlyUnstable": 0,
-    "smRotSymmetrical": 0,
-    "smArSymmetry": 0,
-    "smMaAsymmetrical": 0,
-    "smSeSymmetrical": 0,
-    "haNone": 0,
-    "haStiJamPossible": 0,
-    "haComPenPossibile": 0,
-    "haSeeSymmetrical": 0,
-    "fjNever": 0,
-    "fjOcasionally": 0,
-    "fjRarely": 0,
-    "fjOften": 0,
-    "acVeryGood": 0,
-    "acGood": 0,
-    "acSatisfactory": 0,
-    "acSufficient": 0,
-    "orNoAxes": 0,
-    "orOneAxis": 0,
-    "orTwoAxes": 0,
-    "orThreeAxes": 0,
-    "jmLinear": 0,
-    "jmRotation": 0,
-    "jmLinearRotatory": 0,
-    "jmPathMovement": 0,
-    "jfNone": 0,
-    "jfLow": 0,
-    "jfMedium": 0,
-    "jfHigh": 0,
-    "jaJoinBasicComp": 0,
-    "jaJoinComp": 0,
-    "jaBasicComp": 0,
-    "jaMoreComp": 0,
-    "ncOneBasicComp": 0,
-    "ncTwoBasicComp": 0,
-    "ncThreeBasicComp": 0,
-    "ncMoreComp": 0,
-    "jsSecAllDirection": 0,
-    "jsGravityFit": 0,
-    "jsGravityRubbing": 0,
-    "jsAdditionalSec": 0,
-    "soNone": 0,
-    "soOne": 0,
-    "soTwo": 0,
-    "soMore": 0,
-    "ecAEleConsumFun": 0
-  }
+  matrixRequest: any = {
+    'fkTbAceSubProLev': this.subprocessId,
+    'dsStable': 0,
+    'dsReducedStability': 0,
+    'dsHandlyStable': 0,
+    'dsUnstable': 0,
+    'stInsensitive': 0,
+    'stHardlySensitive': 0,
+    'stSensitive': 0,
+    'stVerySensitive': 0,
+    'grExGripSurface': 0,
+    'grIntGripSurface': 0,
+    'grMagneticGripper': 0,
+    'grFabClosureGripper': 0,
+    'nvNoFurtherVariants': 0,
+    'nvOneFurtherVariants': 0,
+    'nvTwoFurtherVariants': 0,
+    'nvMoreFurtherVariants': 0,
+    'spUp': 0,
+    'spMore': 0,
+    'spStableUnstable': 0,
+    'spOnlyUnstable': 0,
+    'smRotSymmetrical': 0,
+    'smArSymmetry': 0,
+    'smMaAsymmetrical': 0,
+    'smSeSymmetrical': 0,
+    'haNone': 0,
+    'haStiJamPossible': 0,
+    'haComPenPossibile': 0,
+    'haSeeSymmetrical': 0,
+    'fjNever': 0,
+    'fjOcasionally': 0,
+    'fjRarely': 0,
+    'fjOften': 0,
+    'acVeryGood': 0,
+    'acGood': 0,
+    'acSatisfactory': 0,
+    'acSufficient': 0,
+    'orNoAxes': 0,
+    'orOneAxis': 0,
+    'orTwoAxes': 0,
+    'orThreeAxes': 0,
+    'jmLinear': 0,
+    'jmRotation': 0,
+    'jmLinearRotatory': 0,
+    'jmPathMovement': 0,
+    'jfNone': 0,
+    'jfLow': 0,
+    'jfMedium': 0,
+    'jfHigh': 0,
+    'jaJoinBasicComp': 0,
+    'jaJoinComp': 0,
+    'jaBasicComp': 0,
+    'jaMoreComp': 0,
+    'ncOneBasicComp': 0,
+    'ncTwoBasicComp': 0,
+    'ncThreeBasicComp': 0,
+    'ncMoreComp': 0,
+    'jsSecAllDirection': 0,
+    'jsGravityFit': 0,
+    'jsGravityRubbing': 0,
+    'jsAdditionalSec': 0,
+    'soNone': 0,
+    'soOne': 0,
+    'soTwo': 0,
+    'soMore': 0,
+    'ecAEleConsumFun': 0
+  };
 
   matrix = initializeMatrix(this.matrixRequest);
 
   actualLoaInfoRequest = {
-    "fkTbAcePhyLoa": "1",
-    "fkTbAceCogLoa": "1",
-    "possibility": false,
-    "bestRange": "1-1",
-    "fkTbAceSubProLev": this.subprocessId
-  }
+    'fkTbAcePhyLoa': '1',
+    'fkTbAceCogLoa': '1',
+    'possibility': false,
+    'bestRange': '1-1',
+    'fkTbAceSubProLev': this.subprocessId
+  };
 
-  loaInfoRequest:any = new Proxy(
+  loaInfoRequest: any = new Proxy(
     this.actualLoaInfoRequest,
     {
       set: function (obj, prop, value) {
         switch (prop) {
-          case "bestRangeMin":
+          case 'bestRangeMin':
+            if (Number.isNaN(Number(value))) {
+              throw 'Invalid parameter';
+            }
             obj.bestRange = obj.bestRange
               .replace(/^(\d+)/, value);
             return value;
-          case "bestRangeMax":
+          case 'bestRangeMax':
+            if (Number.isNaN(Number(value))) {
+              throw 'Invalid parameter';
+            }
             obj.bestRange = obj.bestRange
               .replace(/(\d+)$/, value);
             return value;
-          case "bestRange":
-            obj.bestRange = obj.bestRange;
+          case 'bestRange':
+            if (/^(\d+)-(\d+)$/.test(value)) {
+              obj.bestRange = obj.bestRange;
+            } else {
+              obj.bestRange = '1-1';
+            }
             return value;
           default:
             return Reflect.set(obj, prop, value);
@@ -126,9 +136,9 @@ export class HTAAnalysisComponent implements OnInit {
       },
       get: function (obj, prop) {
         switch (prop) {
-          case "bestRangeMin":
+          case 'bestRangeMin':
             return /^(\d+)/.exec(obj.bestRange)[0].toString();
-          case "bestRangeMax":
+          case 'bestRangeMax':
             return /(\d+)$/.exec(obj.bestRange)[0].toString();
           default:
             return Reflect.get(obj, prop);
@@ -144,49 +154,50 @@ export class HTAAnalysisComponent implements OnInit {
 
   ngOnInit() {
     this.http
-      .get(environment.apiUrl + "/v1/criteria-matrices-by-subprocess-id/" + this.subprocessId)
+      .get(environment.apiUrl + '/v1/criteria-matrices-by-subprocess-id/' + this.subprocessId)
       .toPromise()
       .catch(
         () => this.http
-          .post(environment.apiUrl + "/v1/criteria-matrices", this.matrixRequest)
+          .post(environment.apiUrl + '/v1/criteria-matrices', this.matrixRequest)
           .toPromise()
       )
       .then(
-        (matrix:any) => {
+        (matrix: any) => {
           this.matrixId = matrix.pkTbId;
           updateMatrix(this.matrix, matrix);
         }
       );
     this.http
-      .get(environment.apiUrl + "/v1/process-loa-info-by-subprocess-id/" + this.subprocessId)
+      .get(environment.apiUrl + '/v1/process-loa-info-by-subprocess-id/' + this.subprocessId)
       .toPromise()
       .catch(
         () => this.http
-          .post(environment.apiUrl + "/v1/process-loa-info", this.actualLoaInfoRequest)
+          .post(environment.apiUrl + '/v1/process-loa-info', this.actualLoaInfoRequest)
           .toPromise()
       )
       .then(
-        (info:any) => {
+        (info: any) => {
           this.infoId = info.pkTbId;
+          this.loaInfoRequest.possibility = info.possibility;
           this.loaInfoRequest.fkTbAcePhyLoa = info.fkTbAcePhyLoa;
           this.loaInfoRequest.fkTbAceCogLoa = info.fkTbAceCogLoa;
-          this.loaInfoRequest.bestRangeMin = info.bestRange;
-          this.loaInfoRequest.bestRangeMax = info.bestRange;
+          this.loaInfoRequest.bestRangeMin = /^(\d+)/.exec(info.bestRange)[0];
+          this.loaInfoRequest.bestRangeMax = /(\d+)$/.exec(info.bestRange)[0];
         }
-      )
+      );
 
-    var cognitiveLoaSubscription:Subscription = this.http
+    const cognitiveLoaSubscription: Subscription = this.http
       .get(`${environment.apiUrl}/v1/cognitive-loa`)
       .subscribe(
-        (cognitiveLoaArray:Array<any>) => {
+        (cognitiveLoaArray: Array<any>) => {
           this.cognitiveLoaArray = cognitiveLoaArray;
           cognitiveLoaSubscription.unsubscribe();
         }
       );
-    var physicalLoaSubscription:Subscription = this.http
+    const physicalLoaSubscription: Subscription = this.http
       .get(`${environment.apiUrl}/v1/physical-loa`)
       .subscribe(
-        (physicalLoaArray:Array<any>) => {
+        (physicalLoaArray: Array<any>) => {
           this.physicalLoaArray = physicalLoaArray;
           physicalLoaSubscription.unsubscribe();
         }
@@ -194,7 +205,7 @@ export class HTAAnalysisComponent implements OnInit {
   }
 
   updateMatrix() {
-    var subscription:Subscription = this.http
+    const subscription: Subscription = this.http
       .put(
         `${environment.apiUrl}/v1/criteria-matrices/${this.matrixId}`,
         this.matrixRequest
@@ -206,13 +217,20 @@ export class HTAAnalysisComponent implements OnInit {
       );
   }
 
-  saveLoaInfo() {
+  saveLoaInfo(min, max) {
+
+    if (min == null && max == null) {
+      console.log('ne moze');
+    } else {
+      console.log('moze');
+    }
+    
     this.http
       .put(
-        environment.apiUrl + "/v1/process-loa-info/" + this.infoId,
+        environment.apiUrl + '/v1/process-loa-info/' + this.infoId,
         this.actualLoaInfoRequest
       )
-      .toPromise()
+      .toPromise();
   }
 
 }
