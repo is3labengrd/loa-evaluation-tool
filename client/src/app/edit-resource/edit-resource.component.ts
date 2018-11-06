f525e9d76b736c764a11ab09f3ab2ac298839014
Drobniac <drobniac@gmail.com>
2018-11-06 12:58:20 +0100

    Integrate resource modification

--- client/src/app/edit-resource/edit-resource.component.ts
+++ client/src/app/edit-resource/edit-resource.component.ts
@@ -1,3 +1,7 @@
+import { environment } from './../../environments/environment';
+import { Router, ActivatedRoute } from '@angular/router';
+import { CookieService } from './../cookie.service';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +11,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditResourceComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private cookieService: CookieService,
+    public router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  resourceId = this.route.snapshot.params["resourceId"];
+
+  selectedSubprocess = this
+    .cookieService
+    .getCookie("selectedSubprocess");
+
+  resource = {
+    "name": null,
+    "loaPhysical": null,
+    "loaCognitive": null,
+    "lcNOperMachine": null,
+    "mcAMaintCosts": null,
+    "mcAMaintCostsPerc": null,
+    "rcInstSurface": null,
+    "rcCostsMMonth": null,
+    "idMacPurhValue": null,
+    "idMacSalesValue": null,
+    "idEcoUsefullLife": null,
+    "icInterRate": null,
+    "ecAEleConsumFun": null,
+    "ecAEleConsumSb": null,
+    "ecElePrice": null
+  }
+
+  resourceInitialState;
 
   ngOnInit() {
+    this.http
+      .get(`${environment.apiUrl}/v1/resources/${this.resourceId}`)
+      .toPromise()
+      .then(
+        (resource) => {
+          this.resourceInitialState = resource;
+          Object.assign(this.resource, this.resourceInitialState);
+        }
+      );
+  }
+
+  cancel = () => {
+    Object.assign(this.resource, this.resourceInitialState);
+  }
+
+  resourceIsIncomplete = () => {
+    return Object.entries(this.resource).some(
+      (keyValuePair) => (keyValuePair[1] == null)
+    );
+  }
+
+  save = () => {
+    return this.http
+      .put(
+        `${environment.apiUrl}/v1/resources/${this.resourceId}`,
+        this.resource
+      )
+      .toPromise()
+      .then(
+        () => {
+          this.router.navigate(["resource-list"]);
+        }
+      )
   }
 
 }
