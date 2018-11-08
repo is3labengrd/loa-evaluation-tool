c373e45871e760dbc8a58f7c872b27f474fe28ff
mmattia17 <mattia.marzano@demetrix.it>
2018-11-08 15:24:53 +0100

    Condition to disallow edit process

--- client/src/app/edit-process/edit-process.component.ts
+++ client/src/app/edit-process/edit-process.component.ts
@@ -39,13 +39,15 @@ export class EditProcessComponent implements OnInit {
   SegmentList:Array<any> = [];
   pks:Array<any> = [];
   cookie: any;
+  disable:boolean;
 
   constructor(private http: HttpClient, public router: Router, private route:ActivatedRoute,private _processListService: CookieService) {}
 
   ngOnInit() {
+      this.disable = false;
+
       this.id  = this.route.snapshot.params['id'];
       this.cookie = this._processListService.getCookie("selectedSubprocess");
-
       var promise2 = this.getSegmentList()
       promise2.then((x) => {
         this.http
@@ -55,10 +57,27 @@ export class EditProcessComponent implements OnInit {
             (main:any) => {
                 this.fetchFromCAM(main);
                 this.mainProcess.push(main);
+                this.getResourceList();
             }
           )
       });
     }
+
+   getResourceList(){
+      return this.http
+        .get(environment.apiUrl + '/v1/subprocess-level-resources')
+        .toPromise()
+        .then(
+          (res:any) => {
+            for(let i in res){
+              if(res[i].subprocessLevel.fkTbAceProSeq === this.SegmentList[0].subProcessLevel1.pkTbId){
+                this.disable = true;
+              }
+            }
+          }
+        )
+    }
+
 
     getSegmentList() {
       return this.http
