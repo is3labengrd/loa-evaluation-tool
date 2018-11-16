4f18a8601d792b500718ca044cdd3deea26b458c
mmattia17 <mattia.marzano@demetrix.it>
2018-11-16 16:56:09 +0100

    Scenarios component Updated

--- client/src/app/scenarios/scenarios.component.ts
+++ client/src/app/scenarios/scenarios.component.ts
@@ -17,64 +17,59 @@ export class ScenariosComponent implements OnInit {
   resSubSceList:Array<any> = [];
   subSceList:Array<any> = [];
   cookie: any;
-  objlist:any={};
-  subProc:any;
-  scenNumber:any;
-  processSegmentList: Array<any> = [];
-  allProcessSegmentCAM: Array<any> = [];
+  processSegmentList:Array<any> = [];
+  allProcessSegmentCAM:Array<any> = [];
   scenarios:any={};
   showAddEdit:any={};
+  scenario1:Array<any> = [];
+  scenario2:Array<any> = [];
+  scenario3:Array<any> = [];
+  resourceTable:Array<any> = [];
 
   ngOnInit() {
     this.cookie = this._processListService.getCookie("selectedSubprocess");
 
     var waitCAMProcTree = this.fetchFromCAM();
-    waitCAMProcTree.then((x)=>{
-      this.findMissingSegments();
-      this.checkduplicate();
-    });
-
     var waitScenarioList = this.getScenariosList();
+    var waitScen = this.getScenarios();
     waitScenarioList.then((x)=>{
-      this.creteTable();
-      this.deleteDupicate();
-      this.getScenarios();
+      waitScen.then((x)=>{
+        this.creteTable();
+        waitCAMProcTree.then((x)=>{
+          this.createResTable();
+        });
+      });
     });
   }
 
   creteTable(){
     for(let i in this.resSubSceList[0]){
-      this.objlist= {};
-      if(this.resSubSceList[0][i].fkTbAceProSeq === this.cookie.mainProcessId){
-        for(let j in this.resSubSceList[0]){
-          if(this.resSubSceList[0][i].subprocessLevel.pkTbId === this.resSubSceList[0][j].subprocessLevel.pkTbId){
-            if(this.resSubSceList[0][j].scenarioNumber === 1){
-              this.objlist['scenNumber1'] = this.resSubSceList[0][j];
-            }
-            if(this.resSubSceList[0][j].scenarioNumber === 2){
-              this.objlist['scenNumber2'] = this.resSubSceList[0][j];
-            }
-            if(this.resSubSceList[0][j].scenarioNumber === 3){
-              this.objlist['scenNumber3'] = this.resSubSceList[0][j];
-            }
+      if(this.resSubSceList[0][i].subprocessLevel.fkTbAceProSeq === this.cookie.mainProcessId){
+
+        if(this.scenarios.scen1 != null){
+          if(this.resSubSceList[0][i].fkTbAceScenarios === this.scenarios.scen1.pkTbId){
+            this.scenario1.push(this.resSubSceList[0][i]);
           }
         }
-        this.subProc = {"subProc": this.resSubSceList[0][i], "objList": this.objlist};
-        this.subSceList.push(this.subProc);
+
+        if(this.scenarios.scen2 != null){
+          if(this.resSubSceList[0][i].fkTbAceScenarios === this.scenarios.scen2.pkTbId){
+            this.scenario2.push(this.resSubSceList[0][i]);
+          }
+        }
+
+        if(this.scenarios.scen3 != null){
+          if(this.resSubSceList[0][i].fkTbAceScenarios === this.scenarios.scen3.pkTbId){
+            this.scenario3.push(this.resSubSceList[0][i]);
+          }
+        }
       }
     }
   }
 
-  deleteDupicate(){
-    var result = this.subSceList.filter(function (a) {
-      return !this[a.subProc.subprocessLevel.pkTbId] && (this[a.subProc.subprocessLevel.pkTbId] = true);
-    }, Object.create(null));
-    this.subSceList = result;
-  }
-
   getScenariosList() {
     return this.http
-    .get(environment.apiUrl + '/v1/subscenarios')
+    .get(environment.apiUrl + '/v1/scenario-resources')
     .toPromise()
     .then(
       (res:any) => {
@@ -95,12 +90,29 @@ export class ScenariosComponent implements OnInit {
     );
   }
 
-  findMissingSegments(){
+  createResTable(){
     for(let i in this.processSegmentList[0]){
       if (this.processSegmentList[0][i].name === this.cookie.mainProcessName){
         for (let j in this.processSegmentList[0][i].subProcesses){
           this.loop(this.processSegmentList[0][i].subProcesses[j].subProcesses);
         }
+      }
+    }
+    var obj = {}
+    for (let k in this.allProcessSegmentCAM){
+      obj = {"name": this.allProcessSegmentCAM[k],
+      "res1": this.findRes(this.scenario1, this.allProcessSegmentCAM[k]),
+      "res2": this.findRes(this.scenario2, this.allProcessSegmentCAM[k]),
+      "res3": this.findRes(this.scenario3, this.allProcessSegmentCAM[k])}
+      this.resourceTable.push(obj);
+      obj = {};
+    }
+  }
+
+  findRes(list, name){
+    for(let i in list){
+      if(list[i].subprocessLevel.name === name){
+        return list[i].resource.name;
       }
     }
   }
@@ -116,42 +128,15 @@ export class ScenariosComponent implements OnInit {
     }
   }
 
-  checkduplicate(){
-    var tmpList = [];
-    var tmpList2 = [];
-
-    for(let i in this.subSceList){
-      tmpList.push(this.subSceList[i].subProc.subprocessLevel.name);
-    }
-
-    for(let i in this.allProcessSegmentCAM){
-      if(!tmpList.includes(this.allProcessSegmentCAM[i])){
-        tmpList2.push(this.allProcessSegmentCAM[i]);
-      }
-    }
-    this.allProcessSegmentCAM = tmpList2;
-  }
-
   getScenarios() {
-
     return this.http
     .get(environment.apiUrl + '/v1/scenarios')
     .toPromise()
     .then(
       (res:any) => {
 
-        var pkSubProc;
-        if(this.cookie.level3 != null){
-          pkSubProc = this.cookie.level3.id;
-        }else{
-          if(this.cookie.level2 != null){
-            pkSubProc = this.cookie.level2.id;
-          }else{
-            pkSubProc = this.cookie.level1.id;
-          }
-        }
         for (let i in res){
-          if(res[i].fkTbAceProSeq === this.cookie.mainProcessId  && res[i].fkTbAceSubProLev === pkSubProc){
+          if(res[i].fkTbAceProSeq === this.cookie.mainProcessId ){
             if(res[i].scenarioNumber === 1){
               this.scenarios['scen1'] = res[i];
             }
@@ -181,6 +166,7 @@ export class ScenariosComponent implements OnInit {
         }else{
           this.showAddEdit['show3'] = false;
         }
+
       });
     }
 
