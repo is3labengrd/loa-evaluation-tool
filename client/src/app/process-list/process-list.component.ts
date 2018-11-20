e0bc9627ff05d1d46d88254c5a6068b717cd8e69
Drobniac <drobniac@gmail.com>
2018-11-20 12:33:54 +0100

    Fix process list level output fix (lol)

--- client/src/app/process-list/process-list.component.ts
+++ client/src/app/process-list/process-list.component.ts
@@ -100,14 +100,12 @@ export class ProcessListComponent implements OnInit {
     this.processSegmentList = this.rawProcessSegmentList.reduce(
       (accumulator: Array<any>, listElement) => {
         let currentListElement = Object.create(null);
-        let subprocessCount = 0;
         let id = listElement.mainProcess ? listElement.mainProcess.pkTbId : "error";
         currentListElement.name = listElement.mainProcess ? listElement.mainProcess.name : "error";
         currentListElement.sub1 = listElement.subProcessLevel1 ? listElement.subProcessLevel1.name : "-";
         currentListElement.sub2 = listElement.subProcessLevel2 ? listElement.subProcessLevel2.name : "-";
         currentListElement.sub3 = listElement.subProcessLevel3 ? listElement.subProcessLevel3.name : "-";
         currentListElement.sublevels = " ";
-        currentListElement.sublevels = subprocessCount;
         currentListElement.editRoute = `/edit-process/${listElement.pkTbId}`;
         if (
           currentListElement.sub1 == '-' &&
@@ -121,6 +119,7 @@ export class ProcessListComponent implements OnInit {
         } else {
           currentListElement.route = '/main-analysis';
           currentListElement.actions = 'Analysis';
+          let subprocessCount = 0;
           for (let property in listElement) {
             if (property.match(/subProcessLevel\d/)) {
               if (listElement[property]) {
@@ -128,6 +127,7 @@ export class ProcessListComponent implements OnInit {
               }
             }
           }
+          currentListElement.sublevels = subprocessCount;
         }
         currentListElement.rawElementReference = listElement;
         accumulator.push(currentListElement);
