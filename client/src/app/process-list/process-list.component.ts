e604079ec1fafcfccf304281b32099fd66f6345e
Drobniac <drobniac@gmail.com>
2018-11-20 12:04:44 +0100

    Fix process list level output

--- client/src/app/process-list/process-list.component.ts
+++ client/src/app/process-list/process-list.component.ts
@@ -100,12 +100,21 @@ export class ProcessListComponent implements OnInit {
     this.processSegmentList = this.rawProcessSegmentList.reduce(
       (accumulator: Array<any>, listElement) => {
         let currentListElement = Object.create(null);
-        let id = listElement.mainProcess ? listElement.mainProcess.pkTbId : "error"
+        let subprocessCount = 0;
+        let id = listElement.mainProcess ? listElement.mainProcess.pkTbId : "error";
         currentListElement.name = listElement.mainProcess ? listElement.mainProcess.name : "error";
-        currentListElement.sublevels = listElement.mainProcess ? listElement.mainProcess.nlowerLevelSubPro : "error";
         currentListElement.sub1 = listElement.subProcessLevel1 ? listElement.subProcessLevel1.name : "-";
         currentListElement.sub2 = listElement.subProcessLevel2 ? listElement.subProcessLevel2.name : "-";
         currentListElement.sub3 = listElement.subProcessLevel3 ? listElement.subProcessLevel3.name : "-";
+        currentListElement.sublevels = " ";
+        for (let property in listElement) {
+          if (property.match(/subProcessLevel\d/)) {
+            if (listElement[property]) {
+              subprocessCount++
+            }
+          }
+        }
+        currentListElement.sublevels = subprocessCount;
         currentListElement.editRoute = `/edit-process/${listElement.pkTbId}`;
         if (
           currentListElement.sub1 == '-' &&
