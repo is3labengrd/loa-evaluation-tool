daaa86b76332a2d0a71eb12cb72495c53c0eee56
domenico-ENG <domenico.martino@eng.it>
2018-11-30 16:51:59 +0100

    Update layout bar chart

--- client/src/app/sub-scenarios-sorting/sub-scenarios-sorting.component.ts
+++ client/src/app/sub-scenarios-sorting/sub-scenarios-sorting.component.ts
@@ -23,51 +23,70 @@ export class SubScenariosSortingComponent implements OnInit {
 
   ngOnInit() {
 
-  this.barChart = new Chart('barChart', {
-                      type: 'bar',
-                       data: {
-                            labels: ['Scenario 1 [...€/piece]', 'Scenario 2 [...€/piece]', 'Scenario 3 [...€/piece]'], // responsible for how many bars are gonna show on the chart
-                            // create 12 datasets, since we have 12 items
-                            // data[0] = labels[0] (data for first bar - 'Standing costs') | data[1] = labels[1] (data for second bar - 'Running costs')
-                            // put 0, if there is no data for the particular bar
-                            datasets: [{
-                              label: 'Physical',
-                              data: [6, 6, 1],
-                              backgroundColor: '#3366cc'
-                            }, {
-                              label: 'Cognitive',
-                              data: [4, 2, 1],
-                              backgroundColor: '#abbcff'
-                            }]
-                         },
-                        options: {
-                                     responsive: false,
-                                     legend: {
-                                        position: 'right', // place legend on the right side of
-                                        labels: {
-                                            padding: 30
-                                        }
-                                      },
-                                     scales: {
-                                        xAxes: [{
-                                           stacked: true, // this should be set to make the bars stacked
-                                           barPercentage: 0.7,
-                                         }],
-                                        yAxes: [{
-                                           stacked: true, // this also..
-                                           scaleLabel: {
-                                                display: true,
-                                                labelString: 'Satisfaction'
-                                           },
-                                           ticks: {
-                                                beginAtZero: true,
-                                                stepSize: 1,
-                                                suggestedMax: 14
-                                           }
-                                         }]
-                                     }
+    this.createBarChart();
+
+  }
+
+
+  createBarChart(){
+      this.barChart = new Chart('barChart', {
+                            type: 'bar',
+                             data: {
+                                  labels: ['Scenario 1 [...€/piece]', 'Scenario 2 [...€/piece]', 'Scenario 3 [...€/piece]'], // responsible for how many bars are gonna show on the chart
+                                  // create 12 datasets, since we have 12 items
+                                  // data[0] = labels[0] (data for first bar - 'Standing costs') | data[1] = labels[1] (data for second bar - 'Running costs')
+                                  // put 0, if there is no data for the particular bar
+                                  datasets: [
+                                  {
+                                    label: 'Minimal total satisfaction',
+                                    data: [7, 7, 7],
+                                    backgroundColor: '#FF0000',
+                                    type: 'line',
+                                    fill: false,
+                                    borderColor: 'red'
+                                  },
+                                  {
+                                    label: 'Physical',
+                                    data: [6, 6, 1],
+                                    backgroundColor: '#3366cc'
+                                  }, {
+                                    label: 'Cognitive',
+                                    data: [4, 2, 1],
+                                    backgroundColor: '#abbcff'
                                   }
-                  });
+                                  ]
+                               },
+                              options: {
+                                           responsive: false,
+                                           legend: {
+                                              position: 'right', // place legend on the right side of
+                                              labels: {
+                                                  padding: 30
+                                              }
+                                            },
+                                           scales: {
+                                              xAxes: [{
+                                                 stacked: true, // this should be set to make the bars stacked
+                                                 barPercentage: 0.7,
+                                               }],
+                                              yAxes: [{
+                                                 stacked: true, // this also..
+                                                 scaleLabel: {
+                                                      display: true,
+                                                      labelString: 'Satisfaction'
+                                                 },
+                                                 ticks: {
+                                                      beginAtZero: true,
+                                                      stepSize: 1,
+                                                      suggestedMax: 14
+                                                 }
+                                               }]
+                                           },
+                                        }
+
+
+                        });
+
   }
 
 }
