3a8b28b10366e600541fe13bc9190c99bafc0fbc
Drobniac <drobniac@gmail.com>
2018-12-05 12:17:28 +0100

    Fix backend issues after introducing cognitive matrix

--- server/src/main/java/eng/it/loatool/subprocess_level/GetSubProcessLevelService.java
+++ server/src/main/java/eng/it/loatool/subprocess_level/GetSubProcessLevelService.java
@@ -6,16 +6,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import eng.it.loatool.process_loa_info.ProcessLOAInformationRepository;
+
 @Service
 public class GetSubProcessLevelService {
 
     @Transactional
-    public Optional<SubProcessLevel> getSubProcessLevel(Integer processId) {
-        return tbAceSubProLevRepository.findById(processId);
+    public Optional<SubProcessLevel> getSubProcessLevel(Integer subprocessId) {
+        Optional<SubProcessLevel> maybeSubprocessLevel = tbAceSubProLevRepository.findById(subprocessId);
+        maybeSubprocessLevel
+            .ifPresent((subprocessLevel) -> {
+                processLOAInformationRepository
+                    .getPhysicalProcessLOAInformationBySubprocessId(subprocessId)
+                    .ifPresent((info) -> {
+                        subprocessLevel.setLoaInfo(info);
+                    });
+            });
+        return maybeSubprocessLevel;
     }
 
+    @Transactional
+    public void addInfoToSubprocessLevel(SubProcessLevel subprocessLevel) {
+        Integer subprocessId = subprocessLevel.getPkTbId();
+        processLOAInformationRepository
+            .getPhysicalProcessLOAInformationBySubprocessId(subprocessId)
+            .ifPresent((info) -> {
+                subprocessLevel.setLoaInfo(info);
+            });
+    }
 
-
+    @Autowired private ProcessLOAInformationRepository processLOAInformationRepository;
     @Autowired private SubProcessLevelRepository tbAceSubProLevRepository;
 
 }
