416daa8243413df3f223068dbbb346cf6de76d2d
Drobniac <drobniac@gmail.com>
2019-05-20 12:10:30 +0200

    Fix resource deletion functionality

--- server/src/main/java/eng/it/loatool/scenario_resource/DeleteScenarioResourceService.java
+++ server/src/main/java/eng/it/loatool/scenario_resource/DeleteScenarioResourceService.java
@@ -2,12 +2,14 @@ package eng.it.loatool.scenario_resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import eng.it.loatool.scenario.ScenarioRepository;
 
 @Service
 public class DeleteScenarioResourceService {
 
+    @Transactional
     public void deleteByResourceId(Integer id) {
         scenarioResourceRepository
             .getScenarioResourcesbyResourceId(id)
